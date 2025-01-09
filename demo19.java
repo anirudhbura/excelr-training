e234915faa6ab0fb4ae077de50174dcd298dc754
anirudhbura <anirudhbura456@gmail.com>
2025-01-09 10:28:50 +0530

    Update demo19.java

--- demo19.java
+++ demo19.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class demo20 {
+public class demo19 {
 
     public static void main(String[] args) {
         // Create a Scanner object to take input from the user
