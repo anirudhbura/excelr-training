312ac425f5c171b019ef33f099c1fe93af174369
anirudhbura <anirudhbura456@gmail.com>
2025-02-12 11:37:38 +0530

    Add files via upload

---
+++ core java/Demo083.java
@@ -0,0 +1,12 @@
+public class Demo083 {
+    public static void main(String[] args) {
+        String name = "Anirudh";
+        String lowerString = name.toLowerCase();
+        char ch = lowerString.charAt(0);
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println("vowel");
+        } else {
+            System.out.println("Not a vowel");
+        }
+    }
+}
